b72f24925003ffb5bb06c9975d7ff18f17a327b4
TyTy <794012191@qq.com>
2023-07-06 19:07:13 +0800

    source codes

---
+++ Source/gametest/gametestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gametestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMETEST_API AgametestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
