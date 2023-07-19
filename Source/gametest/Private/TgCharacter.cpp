9b860d60c59ab274b750b9e6bceaa8631840a57c
TyTy <794012191@qq.com>
2023-07-19 19:44:04 +0800

    7-19晚

--- Source/gametest/Private/TgCharacter.cpp
+++ Source/gametest/Private/TgCharacter.cpp
@@ -4,6 +4,7 @@
 #include "TgCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 ATgCharacter::ATgCharacter()
@@ -12,10 +13,13 @@ ATgCharacter::ATgCharacter()
 	PrimaryActorTick.bCanEverTick = true;
 
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
-	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->bUsePawnControlRotation = true; //将弹簧臂的使用pawn旋转 选项开启
+	SpringArmComp->SetupAttachment(RootComponent); //将弹簧臂绑定到根骨骼上
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
-	CameraComp->SetupAttachment(SpringArmComp);
+	CameraComp->SetupAttachment(SpringArmComp); //将相机绑定到弹簧臂上
 
+	GetCharacterMovement()->bOrientRotationToMovement = true;  // pawn方向会倾向于朝着视角方向 
+	bUseControllerRotationYaw = false;//将控制视角旋转选项 关闭
 }
 
 // Called when the game starts or when spawned
@@ -27,7 +31,23 @@ void ATgCharacter::BeginPlay()
 
 void ATgCharacter::MoveForward(float Value)
 {
-	AddMovementInput(GetActorForwardVector(), Value); //沿着任意向量移动角色，GetActorForwardVector 获取角色向前方向的向量，Value加权值
+	FRotator ControlRot = GetControlRotation();//制作一个旋转器，可获得视角方向
+	ControlRot.Pitch = 0.0f;//屏蔽pitch，roll的值
+	ControlRot.Roll = 0.0f;
+
+	AddMovementInput(ControlRot.Vector(), Value); //沿着任意向量移动角色，ControlRot.Vector() 获取旋转器的方向
+}
+
+
+
+void ATgCharacter::MoveRight(float Value)
+{
+	FRotator ControlRot = GetControlRotation();//制作一个旋转器，可获得视角方向
+	ControlRot.Pitch = 0.0f;//屏蔽pitch，roll的值
+	ControlRot.Roll = 0.0f;
+	FVector RightVecotor = FRotationMatrix(ControlRot).GetScaledAxis(EAxis::Y); 
+	//FRotationMatrix将某种旋转转换为旋转矩阵，乘以坐标轴，即可计算出当前视角下，Y轴的方向
+	AddMovementInput(RightVecotor, Value);
 }
 
 // Called every frame
@@ -43,6 +63,8 @@ void ATgCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis("MoveForward",this, &ATgCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ATgCharacter::MoveRight);
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
